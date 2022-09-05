36749b31da668a937c04f1af1dca3e6985f9e0ca
kidclone3 <kid.clone.3@gmail.com>
2022-09-06 00:53:06 +0700

    Update template

--- template/template.cpp
+++ template/template.cpp
@@ -1,12 +1,18 @@
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// #include<bits/extc++.h>
 
 using namespace std;
-using namespace __gnu_pbds;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag,
+// tree_order_statistics_node_update> ordered_tree; typedef tree<int, null_type,
+// less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+// multi_ordered_tree; tree.find_by_order(x); tree.order_of_key(x); remove
+// element in multi_ordered_tree: tree.erase(--tree.lower_bound(x));
 #define ar array
 #define vt vector
-#define all(v) (v).begin(), (v).end()
+#define all(v) begin(v), end(v)
 #define pb push_back
 #define ll long long
 #define ld long double
@@ -23,8 +29,9 @@ using namespace __gnu_pbds;
 #define vii vt<ii>
 #define fi first
 #define se second
-#define FORIT(i, s) for (auto it=(s.begin()); it!=(s.end()); ++it)
-#define F_OR(i, a, b, s) for (int i=(a); (s)>0? i<(int)(b) : i>(int)(b); i+=(s))
+#define FORIT(i, s) for (auto it = (s.begin()); it != (s.end()); ++it)
+#define F_OR(i, a, b, s)                                                       \
+  for (int i = (a); (s) > 0 ? i < (int)(b) : i > (int)(b); i += (s))
 #define F_OR1(n) F_OR(i, 0, n, 1)
 #define F_OR2(i, e) F_OR(i, 0, e, 1)
 #define F_OR3(i, b, e) F_OR(i, b, e, 1)
@@ -32,44 +39,38 @@ using namespace __gnu_pbds;
 #define GET5(a, b, c, d, e, ...) e
 #define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
 #define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
-#define FOR1(n) F_OR(i, 1, n+1, 1)
-#define EACH(x, a) for(auto& x: a)
+#define FOR1(n) F_OR(i, 1, n + 1, 1)
+#define EACH(x, a) for (auto &x : a)
 
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IOS                                                                    \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
 
-const int d4x[] = {-1, 0, 1, 0},
-        d4y[] = {0, -1, 0, 1},
-        d8x[] = {-1, -1, -1, 0, 0, 1, 1, 1},
-        d8y[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-template <class T>
-void print(T &x)
-{
-    for (auto &it : x)
-    {
-        cerr << it << " ";
-    }
-    cerr << "\n";
+const int d4x[] = {-1, 0, 1, 0}, d4y[] = {0, -1, 0, 1},
+          d8x[] = {-1, -1, -1, 0, 0, 1, 1, 1},
+          d8y[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+template <class T> void print(T &x) {
+  for (auto &it : x) {
+    cerr << it << " ";
+  }
+  cerr << "\n";
 };
-template <class T>
-void printPair(T &x)
-{
-    for (auto &it : x)
-    {
-        cerr << "(" << it.first << ", " << it.second <<") ";
-    }
-    cerr << "\n";
+template <class T> void printPair(T &x) {
+  for (auto &it : x) {
+    cerr << "(" << it.first << ", " << it.second << ") ";
+  }
+  cerr << "\n";
 };
-int solve() {
-    return 0;
-}
+int solve() { return 0; }
 int main() {
-    IOS;
+  IOS;
 #ifndef ONLINE_JUDGE
-	freopen("in", "r", stdin);
-	freopen("out", "w", stdout);
+  freopen("in", "r", stdin);
+  freopen("out", "w", stdout);
 #else
-	// online submission
+  // online submission
 #endif
-    solve();
-    return 0; 
+  solve();
+  return 0;
 }
