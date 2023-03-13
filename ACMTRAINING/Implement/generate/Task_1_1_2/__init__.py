59437ef2b4fbfff3f713a0d2145512b4c4525721
kidclone3 <kid.clone.3@gmail.com>
2023-03-13 10:15:00 +0700

    remove generated testcases

--- ACMTRAINING/Implement/generate/Task_1_1_2/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent))
-import utils
-
-from conf import *
--- ACMTRAINING/Implement/generate/Task_1_1_2/conf.py
+++
@@ -1,5 +0,0 @@
-start_part_1 = 1
-end_part_1 = 11
-
-start_part_2 = 11
-end_part_2 = 51
